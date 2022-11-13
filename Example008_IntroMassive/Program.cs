fda9af3429c420cdd37986fe959042e092d9658d
Denis Myagkov <tomich-84@mail.ru>
2022-11-13 21:08:36 +0300

    add IntroMethod add.

--- Example008_IntroMassive/Program.cs
+++ Example008_IntroMassive/Program.cs
@@ -15,11 +15,14 @@ int c2 = 33;
 int a3 = 15;
 int b3 = 23;
 int c3 = 33;
-
+/*
 int max1 = Max(a1, b1, c1);
 int max2 = Max(a2, b2, c2);
 int max3 = Max(a3, b3, c3);
 int max = Max(max1, max2, max3);
+*/
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 
 // int max = a1;
 // if (b1 > max) max = b1;
